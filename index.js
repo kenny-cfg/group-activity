aacef778854fdd82a8691f7ca982689e86be5a18
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-08 19:46:59 +0100

    Listening on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
