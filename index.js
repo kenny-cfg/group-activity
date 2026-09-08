ed3444442d774ec277a08394fd9d71d273057ea7
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-08 19:49:00 +0100

    GET /hello working

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello!'});
+})
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
